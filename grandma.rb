9f6cd0a6e0cca26fa5999fa3f73c52d96f59dc29
Jordan Harris <jharris@wework.com>
2018-06-21 20:32:34 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,9 +15,9 @@
 def speak_to_grandma(speak)
   if speak == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif speak == speak.upcase 
+  elsif speak != speak.upcase 
     return "HUH?! SPEAK UP, SONNY!"
-  else speak == "HUH?! SPEAK UP, SONNY!" || speak == "WHAT?"
+  else speak == "WHAT DID YOU EAT TODAY?!" || speak == "WHAT?"
     return"NO, NOT SINCE 1938!"
   end
 end
